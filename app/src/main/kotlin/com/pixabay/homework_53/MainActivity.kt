733552fa4681aa7b5e609845df1fb598d958fe17
adzhibaevtan <septempunctan@gmail.com>
2023-01-08 21:39:34 +0600

    Initial commit

---
+++ app/src/main/kotlin/com/pixabay/homework_53/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pixabay.homework_53
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.love.homework_53.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
